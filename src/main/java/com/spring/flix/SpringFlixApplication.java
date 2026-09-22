69fcbc907e81db238d1862e3ab4c6b6478a0f8ac
Laboratorio R2 - 008 <lab008r2@lab.unesc.net>
2026-09-22 19:25:51 -0300

    first commit

---
+++ src/main/java/com/spring/flix/SpringFlixApplication.java
@@ -0,0 +1,11 @@
+package com.spring.flix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlixApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFlixApplication.class, args);
+    }
+}
---
+++ src/test/java/com/spring/flix/SpringFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.flix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
